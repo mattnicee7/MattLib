da2e8f04e4cd3813aeaf9647f3a5037f4a7f63e5
Matheus Colin <mattxnice@gmail.com>
2022-02-28 21:52:18 -0300

    added qr code module

---
+++ src/main/java/com/github/mattnicee7/qrcode/QRCodeReader.java
@@ -0,0 +1,68 @@
+/*
+ * This file is part of MattLib, licensed under the MIT License.
+ *
+ *  Copyright (c) mattnicee7
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package com.github.mattnicee7.qrcode;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import org.jetbrains.annotations.Nullable;
+
+import javax.imageio.ImageIO;
+import java.io.FileInputStream;
+
+/**
+ * Utility class to read QRCodes.
+ */
+public class QRCodeReader {
+
+    /**
+     * Read the QRCode image.
+     *
+     * @param path
+     *        Image file path of QRCode.
+     *
+     * @return The content of qr code.
+     */
+    @Nullable
+    public static String readQRCode(String path) {
+        try {
+            BinaryBitmap binaryBitmap = new BinaryBitmap(
+                    new HybridBinarizer(
+                            new BufferedImageLuminanceSource(
+                                    ImageIO.read(
+                                            new FileInputStream(path)))));
+
+            Result result = new MultiFormatReader().decode(binaryBitmap);
+            return result.getText();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return null;
+    }
+
+}

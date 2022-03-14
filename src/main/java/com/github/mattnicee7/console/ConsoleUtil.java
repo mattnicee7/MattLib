a7c9ae8a1c5b185595eac9d971a2fe92e3469041
Matheus Colin <mattxnice@gmail.com>
2022-03-14 04:41:34 -0300

    added ConsoleUtil

---
+++ src/main/java/com/github/mattnicee7/console/ConsoleUtil.java
@@ -0,0 +1,19 @@
+package com.github.mattnicee7.console;
+
+public class ConsoleUtil {
+
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
+    public static void clear() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+}

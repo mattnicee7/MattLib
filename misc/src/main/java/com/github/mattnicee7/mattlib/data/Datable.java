98aed81c9226e8d98114601d921e3a0db9302adc
mattnicee7 <mattxnice@gmail.com>
2022-04-28 00:12:37 -0300

    added Data and refactored Datable

--- misc/src/main/java/com/github/mattnicee7/mattlib/data/Datable.java
+++ misc/src/main/java/com/github/mattnicee7/mattlib/data/Datable.java
@@ -24,27 +24,11 @@
 
 package com.github.mattnicee7.mattlib.data;
 
-import java.util.HashMap;
-import java.util.Map;
+import lombok.Getter;
 
 public abstract class Datable {
 
-    private final Map<Object, Object> data = new HashMap<>();
-
-    public void set(Object key, Object value) {
-        data.put(key, value);
-    }
-
-    public Object get(Object key) {
-        return data.get(key);
-    }
-
-    public void clear() {
-        data.clear();
-    }
-
-    public boolean containsKey(Object key) {
-        return data.containsKey(key);
-    }
+    @Getter
+    private final Data data = new Data();
 
 }
